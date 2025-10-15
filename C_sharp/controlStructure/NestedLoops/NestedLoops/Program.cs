5dcc83c70e02363d7b12c31ab46188242429d6d2
pouyandeh <pouyandehpour@gmail.com>
2025-10-15 08:09:16 +0330

    C#_CS_NestedLoops_2

--- C_sharp/controlStructure/NestedLoops/NestedLoops/Program.cs
+++ C_sharp/controlStructure/NestedLoops/NestedLoops/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+
+            Console.Write("Enter diamond half height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            // Upper part of diamond
+            for (int row = 1; row <= height; row++)
+            {
+                // Print leading spaces
+                for (int space = 1; space <= height - row; space++)
+                {
+                    Console.Write("  ");
+                }
+
+                // Print increasing numbers
+                for (int num = 1; num <= row; num++)
+                {
+                    Console.Write(num + " ");
+                }
+
+                // Print decreasing numbers
+                for (int num = row - 1; num >= 1; num--)
+                {
+                    Console.Write(num + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            // Lower part of diamond
+            for (int row = height - 1; row >= 1; row--)
+            {
+                // Print leading spaces
+                for (int space = 1; space <= height - row; space++)
+                {
+                    Console.Write("  ");
+                }
+
+                // Print increasing numbers
+                for (int num = 1; num <= row; num++)
+                {
+                    Console.Write(num + " ");
+                }
+
+                // Print decreasing numbers
+                for (int num = row - 1; num >= 1; num--)
+                {
+                    Console.Write(num + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
