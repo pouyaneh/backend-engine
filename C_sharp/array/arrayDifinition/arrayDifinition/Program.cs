1649141436698ba15e81c15460e0d0eb9a3a5293
pouyandeh <pouyandehpour@gmail.com>
2025-10-21 17:46:08 +0330

    C#_array_Difinition

---
+++ C_sharp/array/arrayDifinition/arrayDifinition/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace arrayDifinition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
