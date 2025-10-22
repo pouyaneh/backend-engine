41de0ffc891936854e83525fd622595b56b6fef6
pouyandeh <pouyandehpour@gmail.com>
2025-10-22 17:29:01 +0330

    C#_array_Difinition_2

--- C_sharp/array/arrayDifinition/arrayDifinition/Program.cs
+++ C_sharp/array/arrayDifinition/arrayDifinition/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+
+            //int[] numbers = new int[10] {1,2,3,4,5,6,7,8,9,10 };
+            //int[] numbers = new int[] {1,2,3,4,5,6,7,8,9,10 };
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //numbers[0] = 10;
+            //numbers[5] = 60;
+            //numbers[9] = 100;
+
+            Console.WriteLine(numbers[9]);
         }
     }
 }
