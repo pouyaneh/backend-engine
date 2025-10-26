53caa2f36a1674790635e25b7d18e254329339c1
pouyandeh <pouyandehpour@gmail.com>
2025-10-26 16:17:53 +0330

    C#_array_foreach_2

--- C_sharp/array/foreach/foreach/Program.cs
+++ C_sharp/array/foreach/foreach/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace foreach;
+﻿namespace _foreach;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
+
+        // var
+        int a = 10;
+        var b = "ali";
+        var c = 12.5f;
+
+        // const
+        const float p = 3.14f;
+
+        //foreach
+        int[] numbers = { 10, 13, 17, 19, 20, 40 };
+        int sum = 0;
+        foreach (var item in numbers)
+        {
+            sum += item;
+        }
+        Console.WriteLine($"sum is : {sum}");
+
     }
 }
