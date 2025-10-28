ede7dd742291ce811aad3e90981e5e09be7a0456
pouyandeh <pouyandehpour@gmail.com>
2025-10-28 19:37:57 +0330

    C#_array_sendToMethod_2

--- C_sharp/array/arrayTomethod/arrayTomethod/Program.cs
+++ C_sharp/array/arrayTomethod/arrayTomethod/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+                int[] scores = { 10, 13, 16, 18, 20, 19 };
+                int result = Sum(scores);
+                int average = result / scores.Length;
+                Console.Write($"average is : {average}");
+            }
+
+            static int Sum(int[] array)
+            {
+                int sum = 0;
+                foreach (var item in array)
+                {
+                    sum += item;
+                }
+                return sum;
+            }
         }
-    }
 }
