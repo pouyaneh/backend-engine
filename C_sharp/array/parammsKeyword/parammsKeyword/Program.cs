15343388b541fdecd739ae5450f34c41e93abaa5
pouyandeh <pouyandehpour@gmail.com>
2025-10-30 18:34:06 +0330

    C#_array_parammsKeyword_2

--- C_sharp/array/parammsKeyword/parammsKeyword/Program.cs
+++ C_sharp/array/parammsKeyword/parammsKeyword/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+
+
+
+            int[] numbers = { 1, 2, 3, 5 };
+            int result1 = Sum(10, numbers);
+            int result2 = Sum(20, 13, 15, 17, 19);
+            Console.WriteLine($"result1 is : {result1}");
+            Console.WriteLine($"result2 is : {result2}");
+        }
+
+        static int Sum(int num1, params int[] array)
+        {
+            int total = 0;
+            foreach (var item in array)
+            {
+                total += item;
+            }
+            return total;
         }
     }
 }
