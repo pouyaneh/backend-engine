fbe1748e3f825cb8b506da02da21ded889d90116
pouyandeh <pouyandehpour@gmail.com>
2025-10-31 20:27:25 +0330

    C#_array_MultiDimensional

---
+++ C_sharp/array/MultiDimensional/MultiDimensional/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiDimensional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
