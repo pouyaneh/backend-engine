cdadbd7e1f808bd6c06d02f816b830c85351201b
pouyandeh <pouyandehpour@gmail.com>
2025-11-01 19:06:10 +0330

    C#_array_MultiDimensional_2

--- C_sharp/array/MultiDimensional/MultiDimensional/Program.cs
+++ C_sharp/array/MultiDimensional/MultiDimensional/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+
+            int[,] array = {
+                { 10, 11 },
+                { 12, 100 },
+                { 14, 15 }
+            };
+
+            //array[0, 0] = 10;
+            //array[0, 1] = 11;
+            //array[1, 0] = 12;
+            //array[1, 1] = 13;
+            //array[2, 0] = 14;
+            //array[2, 1] = 15;
+
+            Console.WriteLine(array[1, 1]);
         }
     }
 }
