4626603eb5170f92b0ad6b4cf7bc222daa168523
pouyandeh <pouyandehpour@gmail.com>
2025-11-04 20:06:44 +0330

    C#_array_arrayPractice_2

--- C_sharp/array/arrayPractice/arrayPractice/Program.cs
+++ C_sharp/array/arrayPractice/arrayPractice/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+
+            int[] respones = { 5, 2, 5, 5, 5, 1, 2, 4, 4, 5, 4, 2, 3, 4, 5, 1, 3, 3, 3, 5 };
+            Console.WriteLine(respones[21]);
+            //int[] frequency = new int[6];
+
+            //foreach (var item in respones)
+            //{
+            //    frequency[item]++;
+            //}
+
+            //Console.WriteLine("Rating       Frequency");
+
+            //for (int i = 1; i < frequency.Length; i++)
+            //{
+            //    Console.WriteLine($"   {i}         {frequency[i]}");
+            //}
         }
     }
 }
