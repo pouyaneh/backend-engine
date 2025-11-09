2297067baaac251dbb384a94c4f7e965b4c2758b
pouyandeh <pouyandehpour@gmail.com>
2025-11-09 18:48:11 +0330

    C#_OOP_DefineClass_4

---
+++ C_sharp/objectOrientedProgramming/Define Class/Define Class/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Define_Class
+{
+    internal class Person
+    {
+        public string name;
+        public string family;
+        public int age;
+        public int mobile;
+
+        public void Speak()
+        {
+            Console.WriteLine($"I am {name} {family} and i am {age} years old");
+        }
+    }
+}
--- C_sharp/objectOrientedProgramming/Define Class/Define Class/Program.cs
+++ C_sharp/objectOrientedProgramming/Define Class/Define Class/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            Person obj1 = new Person();
+            obj1.name = "Ali";
+            obj1.family = "Daei";
+            obj1.age = 45;
+
+            obj1.Speak();
         }
     }
 }
