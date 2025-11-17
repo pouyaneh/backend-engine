bb4fd6318b64ce904a7e045386e5997b8152e135
pouyandeh <pouyandehpour@gmail.com>
2025-11-17 19:03:39 +0330

    C#_OOP_Field&Property_7

--- C_sharp/objectOrientedProgramming/Field&Property/Field&Property/Program.cs
+++ C_sharp/objectOrientedProgramming/Field&Property/Field&Property/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Field_Property
+﻿using Classes;
+using System;
+
+namespace Field_Property
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
+            Person obj1 = new Person();
+            obj1.Name = "Ali";
+            obj1.Family = "Daei";
+            obj1.Age = -45;
+
+
+            Console.WriteLine($"Family is : {obj1.Family}");
+            obj1.Speak();
+
+
         }
     }
 }
---
+++ C_sharp/objectOrientedProgramming/Field&Property/Field&Property/person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        // Field (Attibute)
+        // Property
+
+        private string name;
+        private string family;
+        private int age;
+        private int mobile;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Family
+        {
+            get { return family; }
+            set
+            {
+                family = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    age = 0;
+                }
+            }
+        }
+
+        public int Mobile
+        {
+            get
+            {
+                return mobile;
+            }
+            set
+            {
+                mobile = value;
+            }
+        }
+
+
+
+        public void Speak()
+        {
+            Console.WriteLine($"I am {name} {family} and i am {age} years old");
+        }
+    }
+}
