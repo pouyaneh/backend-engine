7ba520afbb6cad75fc96fa2bc08225486034c1c4
pouyandeh <pouyandehpour@gmail.com>
2025-11-18 20:24:21 +0330

    C#_OOP_autoImplementedProperty

---
+++ C_sharp/objectOrientedProgramming/Auto Implemented Property/Auto Implemented Property/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Auto_Implemented_Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
