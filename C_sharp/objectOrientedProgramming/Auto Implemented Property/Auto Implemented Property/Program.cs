3c9cf454a13246a28aeb4494ca6e07e65225c0c2
pouyandeh <pouyandehpour@gmail.com>
2025-11-24 10:49:30 +0330

    C#_OOP_autoImplementedProperty_8

--- C_sharp/objectOrientedProgramming/Auto Implemented Property/Auto Implemented Property/Program.cs
+++ C_sharp/objectOrientedProgramming/Auto Implemented Property/Auto Implemented Property/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-           //Console.WriteLine("Hello, World!");
+            Person obj1 = new Person();
+            obj1.Name = "Ali";
+            obj1.Family = "Daei";
+            obj1.Age = -45;
+
+            Console.WriteLine($"Family is : {obj1.Family}");
+
+            obj1.Speak();
         }
     }
 }
