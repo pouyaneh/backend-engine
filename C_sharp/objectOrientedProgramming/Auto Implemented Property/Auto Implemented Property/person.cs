0de448c62d805e41a2b1f2ac462ff3d2a38e8b60
pouyandeh <pouyandehpour@gmail.com>
2025-11-24 10:47:29 +0330

    C#_OOP_autoImplementedProperty_7

---
+++ C_sharp/objectOrientedProgramming/Auto Implemented Property/Auto Implemented Property/person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Implemented_Property
+{
+    public class Person
+    {
+        // Auto Implemented Property
+
+        private int age;
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value > 0 ? value : 0;
+            }
+        }
+
+        public int Mobile { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine($"I am {Name} {Family} and i am {Age} years old");
+        }
+    }
+}
