fd3c78be8d0f0a8ae57c770b9d1818f9583512a5
pouyandeh <pouyandehpour@gmail.com>
2025-11-25 19:24:04 +0330

    C#_OOP_ObjectInitializerSyntax

---
+++ C_sharp/objectOrientedProgramming/Object Initializer Syntax/Object Initializer Syntax/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Object_Initializer_Syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
