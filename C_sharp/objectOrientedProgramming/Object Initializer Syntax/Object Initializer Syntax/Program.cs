b14ab2309db64a9234ea0eddc271b9c05b625414
pouyandeh <pouyandehpour@gmail.com>
2025-12-16 06:27:41 +0330

    C#_OOP_ObjectInitializerSyntax_20

--- C_sharp/objectOrientedProgramming/Object Initializer Syntax/Object Initializer Syntax/Program.cs
+++ C_sharp/objectOrientedProgramming/Object Initializer Syntax/Object Initializer Syntax/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Object_Initializer_Syntax
+﻿using System;
+
+namespace Classes
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
+            // Object Initializer Syntax
+
+            Person obj1 = new Person() { Name = "Karim", Age = 50, Family = "Bagheri" };
+
+            Console.WriteLine($"Family is : {obj1.Family}");
+
+            obj1.Speak();
+
         }
     }
 }
